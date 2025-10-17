63b86fcf99986b07c7ee7d1458f21d251a316ddd
Jhurta952 <camachohurtado3@gmail.com>
2025-10-17 18:56:42 -0500

    Agregar clase abstracta Figura con método Dibujar

--- Figuras Geometricas/Entidades/Figura.cs
+++ Figuras Geometricas/Entidades/Figura.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+
 
 namespace Figuras_Geometricas.Entidades
 {
-    internal class Figura
+    public abstract class Figura
     {
+        public Color Color { get; set; }
+        public int x { get; set; }
+        public int y { get; set; }
+        public int tamano { get; set; }
+
+        protected Figura(Color color, int x, int y, int tamano)
+        {
+            Color = color;
+            this.x = x;
+            this.y = y;
+            this.tamano = tamano;
+        }
+
+        public abstract void Dibujar(Graphics g);
     }
 }
