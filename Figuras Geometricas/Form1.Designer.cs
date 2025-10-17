07ee105fac4159bf65d7be68613496633b9b2e56
Jhurta952 <camachohurtado3@gmail.com>
2025-10-17 18:08:42 -0500

    Diseñar interfaz gráfica con controles principales

--- Figuras Geometricas/Form1.Designer.cs
+++ Figuras Geometricas/Form1.Designer.cs
@@ -28,30 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            comboBox1 = new ComboBox();
+            cmbFigura = new ComboBox();
+            nudXnudY = new NumericUpDown();
+            nudTamaño = new NumericUpDown();
+            nudX2nudY2 = new NumericUpDown();
+            pbLienzo = new PictureBox();
+            pbColor = new PictureBox();
+            colorDialog1 = new ColorDialog();
+            btnCrear = new Button();
+            btnLimpiar = new Button();
+            txtContador = new TextBox();
+            ((System.ComponentModel.ISupportInitialize)nudXnudY).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudTamaño).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nudX2nudY2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbLienzo).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbColor).BeginInit();
             SuspendLayout();
             // 
-            // comboBox1
+            // cmbFigura
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(70, 58);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(151, 28);
-            comboBox1.TabIndex = 0;
+            cmbFigura.FormattingEnabled = true;
+            cmbFigura.Location = new Point(70, 58);
+            cmbFigura.Name = "cmbFigura";
+            cmbFigura.Size = new Size(151, 28);
+            cmbFigura.TabIndex = 0;
+            // 
+            // nudXnudY
+            // 
+            nudXnudY.Location = new Point(71, 121);
+            nudXnudY.Name = "nudXnudY";
+            nudXnudY.Size = new Size(150, 27);
+            nudXnudY.TabIndex = 1;
+            // 
+            // nudTamaño
+            // 
+            nudTamaño.Location = new Point(71, 190);
+            nudTamaño.Name = "nudTamaño";
+            nudTamaño.Size = new Size(150, 27);
+            nudTamaño.TabIndex = 2;
+            // 
+            // nudX2nudY2
+            // 
+            nudX2nudY2.Location = new Point(71, 257);
+            nudX2nudY2.Name = "nudX2nudY2";
+            nudX2nudY2.Size = new Size(150, 27);
+            nudX2nudY2.TabIndex = 3;
+            // 
+            // pbLienzo
+            // 
+            pbLienzo.BorderStyle = BorderStyle.Fixed3D;
+            pbLienzo.Location = new Point(354, 12);
+            pbLienzo.Name = "pbLienzo";
+            pbLienzo.Size = new Size(400, 600);
+            pbLienzo.TabIndex = 4;
+            pbLienzo.TabStop = false;
+            // 
+            // pbColor
+            // 
+            pbColor.BorderStyle = BorderStyle.Fixed3D;
+            pbColor.Location = new Point(760, 12);
+            pbColor.Name = "pbColor";
+            pbColor.Size = new Size(400, 600);
+            pbColor.TabIndex = 5;
+            pbColor.TabStop = false;
+            // 
+            // btnCrear
+            // 
+            btnCrear.Location = new Point(12, 470);
+            btnCrear.Name = "btnCrear";
+            btnCrear.Size = new Size(137, 52);
+            btnCrear.TabIndex = 6;
+            btnCrear.Text = "Crear";
+            btnCrear.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Location = new Point(185, 470);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(137, 52);
+            btnLimpiar.TabIndex = 7;
+            btnLimpiar.Text = "Limpiar";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            // 
+            // txtContador
+            // 
+            txtContador.Location = new Point(70, 320);
+            txtContador.Name = "txtContador";
+            txtContador.Size = new Size(125, 27);
+            txtContador.TabIndex = 8;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(comboBox1);
+            ClientSize = new Size(1181, 685);
+            Controls.Add(txtContador);
+            Controls.Add(btnLimpiar);
+            Controls.Add(btnCrear);
+            Controls.Add(pbColor);
+            Controls.Add(pbLienzo);
+            Controls.Add(nudX2nudY2);
+            Controls.Add(nudTamaño);
+            Controls.Add(nudXnudY);
+            Controls.Add(cmbFigura);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)nudXnudY).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudTamaño).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nudX2nudY2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbLienzo).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbColor).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private ComboBox comboBox1;
+        private ComboBox cmbFigura;
+        private NumericUpDown nudXnudY;
+        private NumericUpDown nudTamaño;
+        private NumericUpDown nudX2nudY2;
+        private PictureBox pbLienzo;
+        private PictureBox pbColor;
+        private ColorDialog colorDialog1;
+        private Button btnCrear;
+        private Button btnLimpiar;
+        private TextBox txtContador;
     }
 }
