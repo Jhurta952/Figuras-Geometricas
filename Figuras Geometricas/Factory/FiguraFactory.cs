ef2adf3524b08358ca76491cbbcce0c59a059ca7
Jhurta952 <camachohurtado3@gmail.com>
2025-10-17 21:33:30 -0500

    Actualizar FiguraFactory para soportar Linea y Triangulo

--- Figuras Geometricas/Factory/FiguraFactory.cs
+++ Figuras Geometricas/Factory/FiguraFactory.cs
@@ -11,7 +11,7 @@ namespace Figuras_Geometricas.Factory
 {
     internal class FiguraFactory
     {
-        public static Figura CrearFigura(string  tipo, Color color, int x, int y, int tamaño)
+        public static Figura CrearFigura(string  tipo, Color color, int x, int y, int tamaño, int x2 = 0, int y2= 0)
         {
             switch (tipo)
             {
@@ -19,8 +19,12 @@ namespace Figuras_Geometricas.Factory
                     return new Rectangulo(color, x, y, tamaño);
                 case "Circulo":
                     return new Circulo(color, x, y, tamaño);
+                case "Triangulo":
+                    return new Triangulo(color, x, y, tamaño);
+                case "Linea":
+                    return new Linea(color, x, y, x2, y2);
                 default:
-                    throw new ArgumentException("Tipo de figura no reconocido");
+                    return null;
             }
         }
     }
