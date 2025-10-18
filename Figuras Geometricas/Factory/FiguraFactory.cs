7bc5d4189fe61d7f59a0bf493cce8f14b6003a2d
Jhurta952 <camachohurtado3@gmail.com>
2025-10-17 19:28:05 -0500

    Implementar clase FiguraFactory para crear figuras

--- Figuras Geometricas/Factory/FiguraFactory.cs
+++ Figuras Geometricas/Factory/FiguraFactory.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using Figuras_Geometricas.Entidades;
+
 
 namespace Figuras_Geometricas.Factory
 {
     internal class FiguraFactory
     {
+        public static Figura CrearFigura(string  tipo, Color color, int x, int y, int tamaño)
+        {
+            switch (tipo)
+            {
+                case "Rectangulo":
+                    return new Rectangulo(color, x, y, tamaño);
+                case "Circulo":
+                    return new Circulo(color, x, y, tamaño);
+                default:
+                    throw new ArgumentException("Tipo de figura no reconocido");
+            }
+        }
     }
 }
