0b0a8d10e43ff7ee7cdb2ae6f443d306fa3cec11
Jhurta952 <camachohurtado3@gmail.com>
2025-10-17 19:20:10 -0500

    Agregar clases Rectangulo y Circulo que heredan de Figura

--- Figuras Geometricas/Entidades/Circulo.cs
+++ Figuras Geometricas/Entidades/Circulo.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Figuras_Geometricas.Entidades
 {
-    internal class Circulo
+    internal class Circulo : Figura
     {
+        public Circulo(Color color, int x, int y, int tamaño) : base(color, x, y, tamaño) { }
+        public override void Dibujar(Graphics g)
+        {
+            using (Brush b = new SolidBrush(this.Color))
+            {
+                g.FillEllipse(b, this.X, this.Y, this.Tamaño, this.Tamaño);
+            }
+            using (Pen p = new Pen(Color.Black, 2))
+            {
+                g.DrawEllipse(p, this.X, this.Y, this.Tamaño, this.Tamaño);
+            }
+        }
     }
 }
--- Figuras Geometricas/Entidades/Rectangulo.cs
+++ Figuras Geometricas/Entidades/Rectangulo.cs
@@ -7,8 +7,22 @@ using System.Drawing;
 
 namespace Figuras_Geometricas.Entidades
 {
-    internal class Rectangulo
+    internal class Rectangulo : Figura
     {
+        public Rectangulo(Color color, int x, int y, int tamaño) : base(color, x, y, tamaño) { }
+
+        public override void Dibujar(Graphics g)
+        {
+            using (Brush b = new SolidBrush(this.Color))
+            {
+                g.FillRectangle(b, this.X, this.Y, this.Tamaño, this.Tamaño);
+            }
+
+            using (Pen p = new Pen(Color.Black, 2))
+            {
+                g.DrawRectangle(p, this.X, this.Y, this.Tamaño, this.Tamaño);
+            }
+        }
     }
 }
 
